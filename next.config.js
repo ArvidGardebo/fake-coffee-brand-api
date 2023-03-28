855bde1bba123418e9722cb58d0b14f7a64f800b
flamingosolid <josefabdo90@gmail.com>
2023-03-28 16:10:18 +0200

    added style components

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const sass = require("sass");
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
+import global from "../styles/global.module.scss";
+import test from "../styles/test.module.scss";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className={global.styles}>
+        <div>
+          <h1>Home</h1>
+          <p>Hejsan</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
