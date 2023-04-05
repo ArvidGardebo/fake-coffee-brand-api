11439e19c4fdd5978a06b6401936031b8b77d06f
joelhols <jholzner@hotmail.se>
2023-04-06 00:08:34 +0200

    live

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 const path = require("path");
 module.exports = {
   publicRuntimeConfig: {
-    // staticFolder: "/static",
     distDir: "build",
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-
-  //   async rewrites() {
-  //     return [
-  //       {
-  //         source: "/api/:id",
-  //         destination: "https://fake-coffe-api.vercel.app//api/:id",
-  //       },
-  //     ];
-  //   },
 };
