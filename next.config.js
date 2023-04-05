79438bea3fc27550b0db48693c8977a330143c2b
joelhols <jholzner@hotmail.se>
2023-04-05 11:38:04 +0200

    v2

--- next.config.js
+++ next.config.js
@@ -2,18 +2,21 @@ const path = require("path");
 module.exports = {
   publicRuntimeConfig: {
     staticFolder: "/static",
+    distDir: "build",
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  experimental: {
-    nftTracing: true,
-  },
+
   async rewrites() {
     return [
       {
+        source: "/api/:path*",
+        destination: "https://fake-coffee-brand-api.vercel.app/:path*",
+      },
+      {
         source: "/static/:path*",
-        destination: `${process.env.NEXT_PUBLIC_STATIC_FOLDER}/:path*`,
+        destination: "/fake-coffee-brand-api/static/:path*",
       },
     ];
   },
